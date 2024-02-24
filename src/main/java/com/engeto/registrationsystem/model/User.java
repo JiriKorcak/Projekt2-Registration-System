c2d4dc5ed5edf2107c495461c24e6b700aa76681
Jiří Korčák <j.korcak@centrum.cz>
2024-02-24 11:58:39 +0100

    Odstranění mrtvého kódu.

--- src/main/java/com/engeto/registrationsystem/model/User.java
+++ src/main/java/com/engeto/registrationsystem/model/User.java
@@ -1,6 +1,5 @@
 package com.engeto.registrationsystem.model;
 
-import com.engeto.registrationsystem.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
